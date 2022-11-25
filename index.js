81e05f23e5540ba869d2865f66d32221671d2cd7
bayajidalam <bayajidalam2001@gmail.com>
2022-11-25 17:31:28 +0600

    post api created

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
+const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@myclaster-1.wxhqp81.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.get('/',(req,res)=>{
-  res.send('Hello')
-})
+async function run(){
 
-app.listen(port,()=>{
-  console.log(`app is listening on port ${port}`)
-})
+  try{
+    
+    // colletions 
+    const categorysCollection = client.db('furniDokan').collection('categorys')
+    const singleCategoryCollection = client.db('furniDokan').collection('singleCategory')
+    const usersCollection = client.db('furniDokan').collection('users')
+
+    // get all categorys 
+    app.get('/categorys',async(req,res)=>{
+      const query = {}
+      const categorys = await categorysCollection.find(query).toArray()
+      res.send(categorys)
+    })
+
+    // get all data of a query 
+    app.get('/category',async(req,res)=>{
+      const name = req.query.name
+      const query = {catName:name}
+      console.log(name)
+      const data = await singleCategoryCollection.find(query).toArray()
+      res.send(data)
+    })
+
+    // save a user to db 
+    app.post('/user',async(req,res)=>{
+      const user = req.body
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+  }
+  finally{
+   
+  }
+}
+run().catch(err=>console.log(err))
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
